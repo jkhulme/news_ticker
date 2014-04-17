d8a6c6653e3f73ad9c3421e6baaef57d00ecfc19
James Hulme <github@jkhulme.fastmail.fm>
2014-04-17 22:18:20 +0100

    styling is a bit better

--- news_ticker.js
+++ news_ticker.js
@@ -12,11 +12,12 @@ function add_ticker(news_feed){
     ticker_div.style.position = "fixed";
     ticker_div.style.width = width() + "px";
     ticker_div.style.height = "20px";
-    ticker_div.style.background = "grey";
+    ticker_div.style.background = "#D1D1D1";
     ticker_div.style.top = height()-20 + "px";
     ticker_div.style.left = "0px";
     ticker_div.style.opacity = 0.8;
-    ticker_div.innerHTML = "<marquee behavior='scroll' direction='right' scrollamount='5'><font color='white'>"+news_feed+"</font></marquee>";
+    ticker_div.style.zIndex = 9999;
+    ticker_div.innerHTML = "<marquee class='news_ticker-7ce5f950-c670-11e3-9c1a-0800200c9a67' behavior='scroll' direction='right' scrollamount='4'><font color='black'>"+news_feed+"</font></marquee>";
     document.body.appendChild(ticker_div);
 }
 
@@ -26,9 +27,10 @@ function build_news_feed(news){
         for (var j=0;j<news[i].length;j++){
             var title = news[i][j][0];
             var link = news[i][j][1];
-            var story = "<a href='"+link+"'>"+title+"</a>";
-            output += story + " ";
+            var story = "<a class='news_ticker-7ce5f950-c670-11e3-9c1a-0800200c9a66' href='"+link+"'>"+title+"</a>";
+            output += story + " || ";
         }
+        output = output.substring(0, output.length - 4);
     }
     return output;
 }
